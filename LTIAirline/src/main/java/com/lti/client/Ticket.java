7feeaa54cc1cf36acdbb37c64e05aba625074899
vikram <markiv1155@gmail.com>
2021-09-15 14:46:18 +0530

    ticket change

--- LTIAirline/src/main/java/com/lti/client/Ticket.java
+++ LTIAirline/src/main/java/com/lti/client/Ticket.java
@@ -3,6 +3,15 @@ package com.lti.client;
 public class Ticket {
 	
 	private int x;
+	private int y;
+
+	public int getY() {
+		return y;
+	}
+
+	public void setY(int y) {
+		this.y = y;
+	}
 
 	public int getX() {
 		return x;
